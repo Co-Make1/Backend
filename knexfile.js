136d0a67f197793ae99f0cb2a36ea14b5e8e9c12
FreedomWriter <natalienetworks@gmail.com>
2020-02-26 10:43:42 -0600

    debugging deployment

--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,59 @@
 module.exports = {
   development: {
     client: "sqlite3",
-    connection: { filename: "./data/comake.db3" },
     useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations",
-      tableName: "dbmigrations"
+    connection: { filename: "./data/comake.db3" },
+    migrations: { directory: "./data/migrations" },
+    seeds: { directory: "./data/seeds" },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
+  },
+
+  staging: {
+    client: "sqlite3",
+    connection: {
+      database: "comake",
+      user: "username",
+      password: "password"
     },
-    seeds: { directory: "./data/seeds" }
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "sqlite3",
+    connection: {
+      database: "comake",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
   },
   testing: {
     client: "sqlite3",
     connection: {
-      filename: "./data/test.db3"
+      filename: "./database/test.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./data/migrations"
+      directory: "./database/migrations"
     },
     seeds: {
-      directory: "./data/seeds"
+      directory: "./database/seeds"
     }
   }
 };
