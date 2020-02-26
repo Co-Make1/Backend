72c40aadbee2f4a8675cb57d79668d8e70da2292
FreedomWriter <natalienetworks@gmail.com>
2020-02-26 10:20:58 -0600

    debugging deploy - knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,46 +1,25 @@
 module.exports = {
   development: {
     client: "sqlite3",
-    useNullAsDefault: true,
     connection: { filename: "./data/comake.db3" },
-    migrations: { directory: "./data/migrations" },
-    seeds: { directory: "./data/seeds" },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
-    }
-  },
-
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    useNullAsDefault: true,
     migrations: {
-      tableName: "knex_migrations"
-    }
+      directory: "./data/migrations",
+      tableName: "dbmigrations"
+    },
+    seeds: { directory: "./data/seeds" }
   },
-
-  production: {
-    client: "postgresql",
+  testing: {
+    client: "sqlite3",
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
+      filename: "./data/test.db3"
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    useNullAsDefault: true,
     migrations: {
-      tableName: "knex_migrations"
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   }
 };
