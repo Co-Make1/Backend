171293c101f3d3bddc3c849ebe9825805eeb8c66
FreedomWriter <natalienetworks@gmail.com>
2020-02-26 09:57:33 -0600

    debugging deploy

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: "./data/comake.db3"
+      filename: Path2D.resolve(__dirname, "./data/comake.db3")
     },
     migrations: {
       directory: "./data/migrations"
